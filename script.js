9c223a54400e885f3d7338799c778ffc695aa150
amabie000 <130695385+amabie000@users.noreply.github.com>
2023-06-07 11:11:08 -0700

    Update script.js
    included form and sheets url id

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/light-v10',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token' // replace with your Mapbox access token
+    accessToken: 'pk.eyJ1IjoiYW1hYmllIiwiYSI6ImNsaWxhZGZ2NDA2c2szcnFwM3M4cndwNWkifQ.c-3RpmSd7TY5cLbS_KF1zQ' // replace with your Mapbox access token
 }).addTo(map);
 
 
@@ -22,9 +22,9 @@ map.on('contextmenu', function(e) {
 
 
     // Construct the prefilled form URL
-    var formUrl = 'https://docs.google.com/forms/d/e/[YourFormID]/viewform?usp=pp_url';
-    formUrl += '&entry.[FieldID1]=' + lat;
-    formUrl += '&entry.[FieldID2]=' + lng;
+    var formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSfSHBM_HSUU5kR--wH2Gbqd2nYaHJ5sdoAgUr1awYZdQ8hWeg/viewform?usp=pp_url&entry.1611770807=a&entry.987147841=a&entry.19781950=2001-01-01&entry.1381225094=2&entry.102830186=3';
+    formUrl += '&entry.[1381225094]=' + lat;
+    formUrl += '&entry.[102830186]=' + lng;
 
 
     // Create a popup at the clicked location with a link to the form
@@ -35,7 +35,7 @@ map.on('contextmenu', function(e) {
 });
 
 // Fetch data from Google Spreadsheet
-fetch('https://docs.google.com/spreadsheets/d/e/[YourPublicFormID]/pub?output=csv')
+fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vRuOsnWzOa4supR892usPTIQZA4bw5twX8IQD-_y1PPQgxsiQ_veDHXkf7RTKsFUBVUFllcZ16L_CJ_/pub?output=csv')
     .then(response => response.text())
     .then(data => {
         // Parse the CSV data
@@ -48,7 +48,7 @@ fetch('https://docs.google.com/spreadsheets/d/e/[YourPublicFormID]/pub?output=cs
             let lat = row['Lat'];
             let lng = row['Long'];
             let name = row['Name'];
-            let experience = row['Share your experience in this place'];
+            let experience = row['What Happened Here'];
 
 
             // Add a marker to the map at the latitude and longitude
